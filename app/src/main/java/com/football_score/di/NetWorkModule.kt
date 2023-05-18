56cb65e7011107279b13892c14306893a4b899ff
Dam Hong Duc <hongduc001h@gmail.com>
2023-05-18 13:54:57 +0700

    feat: increate kotlin version

--- app/src/main/java/com/football_score/di/NetWorkModule.kt
+++ app/src/main/java/com/football_score/di/NetWorkModule.kt
@@ -1,44 +1,44 @@
-//package com.football_score.di
-//
-//import com.football_score.constant.CommonConstants
-//import com.football_score.data.remote.service.FootballAPIService
-//import com.football_score.utils.RequestInterceptor
-//import dagger.Module
-//import dagger.Provides
-//import dagger.hilt.InstallIn
-//import dagger.hilt.components.SingletonComponent
-//import okhttp3.OkHttpClient
-//import okhttp3.logging.HttpLoggingInterceptor
-//import retrofit2.Retrofit
-//import retrofit2.converter.gson.GsonConverterFactory
-//
-//@Module
-//@InstallIn(SingletonComponent::class)
-//class NetWorkModule {
-//    @Provides
-//    fun okHttp(): OkHttpClient {
-//        val loggingInteceptor = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
-//
-//        return OkHttpClient.Builder()
-//            .addInterceptor(loggingInteceptor)
-//            .addInterceptor(RequestInterceptor())
-//            .build();
-//
-//    }
-//
-//    @Provides
-//    fun retrofit(okHttpClient: OkHttpClient): Retrofit
-//    {
-//        return Retrofit.Builder().
-//        client(okHttpClient).
-//        addConverterFactory(GsonConverterFactory.create()).
-//        baseUrl(CommonConstants.BASE_URL).
-//        build();
-//    }
-//
-//    @Provides
-//    fun FootballAPIService(retrofit: Retrofit): FootballAPIService
-//    {
-//        return retrofit.create(FootballAPIService::class.java)
-//    }
-//}
+package com.football_score.di
+
+import com.football_score.constant.CommonConstants
+import com.football_score.data.remote.service.FootballAPIService
+import com.football_score.utils.RequestInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetWorkModule {
+    @Provides
+    fun okHttp(): OkHttpClient {
+        val loggingInteceptor = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInteceptor)
+            .addInterceptor(RequestInterceptor())
+            .build();
+
+    }
+
+    @Provides
+    fun retrofit(okHttpClient: OkHttpClient): Retrofit
+    {
+        return Retrofit.Builder().
+        client(okHttpClient).
+        addConverterFactory(GsonConverterFactory.create()).
+        baseUrl(CommonConstants.BASE_URL).
+        build();
+    }
+
+    @Provides
+    fun FootballAPIService(retrofit: Retrofit): FootballAPIService
+    {
+        return retrofit.create(FootballAPIService::class.java)
+    }
+}
