48a00a9e0fc31b051db3cc0f018f55e797dfe3a5
Dam Hong Duc <hongduc001h@gmail.com>
2023-05-29 21:16:07 +0700

    wip

--- app/src/main/java/com/football_score/data/remote/FootballAPIService.kt
+++ app/src/main/java/com/football_score/data/remote/FootballAPIService.kt
@@ -1,10 +1,11 @@
 package com.football_score.data.remote
 
+import com.football_score.data.remote.entity.MatchResponseAPIEntity
 import com.football_score.utils.EndPoint
 import com.football_score.domain.model.MatchResponse
 import retrofit2.http.GET
 
 interface FootballAPIService {
     @GET(EndPoint.GET_ALL_LIVE_MATCH)
-    suspend fun getAllLiveMatch(): MatchResponse
+    suspend fun getAllLiveMatch(): MatchResponseAPIEntity
 }
--- app/src/main/java/com/football_score/data/repository/MatchRepositoryImpl.kt
+++ app/src/main/java/com/football_score/data/repository/MatchRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.football_score.data.repository
 
 import com.football_score.data.remote.FootballAPIService
+import com.football_score.data.remote.entity.toDomain
 import com.football_score.domain.model.MatchResponse
 import com.football_score.domain.repository.MatchRepository
 import javax.inject.Inject
@@ -8,5 +9,5 @@ import javax.inject.Inject
 class MatchRepositoryImpl @Inject constructor(private val footballAPIService: FootballAPIService) :
     MatchRepository {
     override suspend fun getAllLiveMatch(): MatchResponse =
-        footballAPIService.getAllLiveMatch();
+        footballAPIService.getAllLiveMatch().toDomain();
 }
