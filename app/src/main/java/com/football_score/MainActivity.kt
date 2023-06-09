ec4ce86ae3f35db7309ab8f1ab086806c8f21a0b
Dam Hong Duc <hongduc001h@gmail.com>
2023-06-09 16:07:05 +0700

    Update MainActivity.kt

--- app/src/main/java/com/football_score/MainActivity.kt
+++ app/src/main/java/com/football_score/MainActivity.kt
@@ -10,6 +10,7 @@ import dagger.hilt.android.AndroidEntryPoint
 
 // architecture: https://paulallies.medium.com/clean-architecture-in-the-flavour-of-jetpack-compose-dd4b0016f815
 // dependencies injection: https://medium.com/@myofficework000/dependency-injection-dagger-hilt-in-android-14a7f03050e8
+// dagger - hilt anotation: https://developer.android.com/static/images/training/dependency-injection/hilt-annotations.pdf
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
