9488bd4881f0e91ef34fcd59e23608893667511f
Dam Hong Duc <hongduc001h@gmail.com>
2023-06-16 16:41:15 +0700

    wip

--- app/src/main/java/com/football_score/presentation/screens/activity/MainActivity.kt
+++ app/src/main/java/com/football_score/presentation/screens/activity/MainActivity.kt
@@ -1,9 +1,10 @@
-package com.football_score
+package com.football_score.presentation.screens.activity
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
+import com.football_score.App
 import com.football_score.navigation.SetUpNavGraph
 import com.football_score.ui.theme.Football_scoreTheme
 import dagger.hilt.android.AndroidEntryPoint
