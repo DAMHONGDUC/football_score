470a1ae5c5470764e068c8b3894f52d2bffe3fdc
Dam Hong Duc <hongduc001h@gmail.com>
2023-06-16 17:14:28 +0700

    wip

--- app/src/main/java/com/football_score/App.kt
+++ app/src/main/java/com/football_score/App.kt
@@ -4,6 +4,17 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 import androidx.compose.runtime.mutableStateOf
 
+// architecture: https://paulallies.medium.com/clean-architecture-in-the-flavour-of-jetpack-compose-dd4b0016f815
+// dependencies injection: https://medium.com/@myofficework000/dependency-injection-dagger-hilt-in-android-14a7f03050e8
+// dagger - hilt anotation: https://developer.android.com/static/images/training/dependency-injection/hilt-annotations.pdf
+
+// @InstallIn: chỉ ra Hilt-generated container
+
+// @Inject - constructor: nói cho dagger-hilt biết cái constructor nào dùng để tạo instance & những dependencies của constructor
+// @Inject - field: ko thể private, có chức năng lấy các field ở các class có @Inject trong class  @AndroidEntryPoint
+
+// https://github.com/piashcse/Hilt-MVVM-Compose-Movie
+
 @HiltAndroidApp
 class App : Application() {
     val isDark = mutableStateOf(false)
--- app/src/main/java/com/football_score/presentation/screens/activity/MainActivity.kt
+++ app/src/main/java/com/football_score/presentation/screens/activity/MainActivity.kt
@@ -10,17 +10,6 @@ import com.football_score.ui.theme.Football_scoreTheme
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
-// architecture: https://paulallies.medium.com/clean-architecture-in-the-flavour-of-jetpack-compose-dd4b0016f815
-// dependencies injection: https://medium.com/@myofficework000/dependency-injection-dagger-hilt-in-android-14a7f03050e8
-// dagger - hilt anotation: https://developer.android.com/static/images/training/dependency-injection/hilt-annotations.pdf
-
-// @InstallIn: chỉ ra Hilt-generated container
-
-// @Inject - constructor: nói cho dagger-hilt biết cái constructor nào dùng để tạo instance & những dependencies của constructor
-// @Inject - field: ko thể private, có chức năng lấy các field ở các class có @Inject trong class  @AndroidEntryPoint
-
-// https://github.com/piashcse/Hilt-MVVM-Compose-Movie
-
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
