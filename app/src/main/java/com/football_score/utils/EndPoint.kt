55ec076cf847d472251e55a449836802047ae02c
Dam Hong Duc <hongduc001h@gmail.com>
2023-06-22 13:25:38 +0700

    wip

--- app/src/main/java/com/football_score/utils/EndPoint.kt
+++ app/src/main/java/com/football_score/utils/EndPoint.kt
@@ -1,14 +1,7 @@
 package com.football_score.utils
 
 object EndPoint {
-    const val GET_ALL_LIVE_MATCH: String = "/fixtures?live=all"
-
-    const val GET_ALL_HOT_MATCH: String = "/fixtures?live=all"
-
-
-
     const val FIXTURES: String = "/fixtures"
-
     const val TEAMS: String = "/teams"
 }
 
