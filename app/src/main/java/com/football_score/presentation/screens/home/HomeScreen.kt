011848e7431c7cc4e27c881a5e4f463bde810a74
Dam Hong Duc <hongduc001h@gmail.com>
2023-06-22 16:55:34 +0700

    add drop down select team

--- app/src/main/java/com/football_score/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/football_score/presentation/screens/home/HomeScreen.kt
@@ -1,17 +1,24 @@
 package com.football_score.presentation.screens.home
 
 import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Autorenew
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import coil.compose.AsyncImage
 import com.football_score.App
 import com.football_score.domain.model.LeagueTeam
 import com.football_score.domain.model.Match
@@ -54,7 +61,7 @@ fun HomeScreen(
                 when (liveMatches) {
                     is ViewModelState.Empty -> Text(text = "No liveMatches data available")
                     is ViewModelState.Loading -> Text(text = "liveMatches Loading...")
-                    is ViewModelState.Success -> LiveMatch(listLiveMatch = liveMatches.data.response!!)
+                    is ViewModelState.Success -> LiveMatch(listLiveMatch = liveMatches.data.response)
                     is ViewModelState.Error -> Text(text = liveMatches.message)
                 }
 
@@ -63,12 +70,79 @@ fun HomeScreen(
                 when (leagueTeams) {
                     is ViewModelState.Empty -> Text(text = "No leagueTeams data available")
                     is ViewModelState.Loading -> Text(text = "leagueTeams Loading...")
-                    is ViewModelState.Success -> HotMatch(listLeagueTeam = leagueTeams.data.response!!)
+                    is ViewModelState.Success -> HotMatch(listLeagueTeam = leagueTeams.data.response)
                     is ViewModelState.Error -> Text(text = leagueTeams.message)
                 }
             }
         }
     )
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun ExposedDropdownLeagueTeam(listLeagueTeam: List<LeagueTeam>) {
+    val context = LocalContext.current
+    var expanded by remember { mutableStateOf(false) }
+    var selectedItem by remember { mutableStateOf(listLeagueTeam[0]) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = {
+                expanded = !expanded
+            }
+        ) {
+            Row() {
+                TextField(
+                    value = selectedItem.team.name,
+                    onValueChange = {},
+                    readOnly = true,
+                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                    leadingIcon = {
+                        AsyncImage(
+                            model = selectedItem.team.logo,
+                            contentDescription = null,
+                            modifier = Modifier
+                                .width(20.dp))
+                    },
+                )
+            }
+
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                listLeagueTeam.forEach { item ->
+                    DropdownMenuItem(
+                        content = {
+                            Row()
+                            {
+                                AsyncImage(
+                                    model = item.team.logo,
+                                    contentDescription = null,
+                                    modifier = Modifier
+                                        .width(20.dp)
+                                        .height((20.dp))
+                                )
+                                Spacer(modifier = Modifier.width(20.dp))
+                                Text(text = item.team.name)
+                            }
+
+                        },
+                        onClick = {
+                            selectedItem = item
+                            expanded = false
+                            Toast.makeText(context, item.team.name, Toast.LENGTH_SHORT).show()
+                        }
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Composable
@@ -109,15 +183,7 @@ fun HotMatch(listLeagueTeam: List<LeagueTeam>) {
         if (listLeagueTeam.isEmpty()) {
             Text(text = "No League Team", modifier = Modifier.padding(10.dp))
         } else {
-            LazyColumn(
-                verticalArrangement = Arrangement.spacedBy(20.dp)
-            )
-            {
-                items(listLeagueTeam.size)
-                {
-                    Text(text = listLeagueTeam[it].team.name)
-                }
-            }
+            ExposedDropdownLeagueTeam(listLeagueTeam = listLeagueTeam)
         }
     }
 }
--- app/src/main/java/com/football_score/utils/Constants.kt
+++ app/src/main/java/com/football_score/utils/Constants.kt
@@ -3,5 +3,7 @@ package com.football_score.utils
 object Constants {
     const val BASE_URL: String = "https://v3.football.api-sports.io"
     const val X_RAPIDAPI_HOST: String = "v3.football.api-sports.io"
-    const val X_RAPIDAPI_KEY: String = "0f5e32071180c89dfb37b31d7e4472c5"
+    //const val X_RAPIDAPI_KEY: String = "0f5e32071180c89dfb37b31d7e4472c5"
+    //const val X_RAPIDAPI_KEY: String = " 3e7eee2b95455986556d8ecbe37b78f7"
+    const val X_RAPIDAPI_KEY: String = " fd86225035b102e7d0a364232d07fb90"
 }
